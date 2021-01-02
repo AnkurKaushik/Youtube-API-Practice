c6f5d05b7278ec13d91204afab2f1d25480788fe
AnkurKaushik <a123ankur@yahoo.com>
2021-01-02 14:33:45 -0600

    added functionality to export title array

--- src/Vid.js
+++ src/Vid.js
@@ -1,9 +1,10 @@
-//import React from 'react';
+import React from 'react';
 import videos from './videos.json';
 
 const vids = []
+const titles = []
 
-
+//this function is now obsolete since we can just add iframes to an array
 /* function addVideo(source2) {
     var vid = document.createElement("iframe");     
     vid.src = source2;   
@@ -12,10 +13,20 @@ const vids = []
     document.body.appendChild(vid);
 } */
 
-
+const videoArry = []
+const titleArry = []
 for(let index = 0; index < 100; index++) {
     //addVideo("https://www.youtube.com/embed/" + videos[index].snippet.resourceId.videoId)
     vids.push(videos[index].snippet.resourceId.videoId);
+    titles.push(videos[index].snippet.title);
 } 
 
-export default vids;
+const key = "https://www.youtube.com/embed/";
+for (const [index, value] of vids.entries()) {
+    titleArry.push(<li key={index}>{titles[index]}</li>)
+    videoArry.push(<iframe title={titles[index]} src={key + value} height="225" width="400"></iframe>)
+}
+
+export {titleArry};
+
+export default videoArry; 
--- src/Videos.js
+++ src/Videos.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import Vid from './Vid';
-
-//var source = "https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com";
-//const s = ['CUwDZLhnfes', 'w_cJYKM7J2Q'];
-const videoArry = []
-
-const key = "https://www.youtube.com/embed/";
-for (const [index, value] of Vid.entries()) {
-    videoArry.push(<li key={index}>{value}</li>)
-    videoArry.push(<iframe title={value} src={key + value} height="225" width="400"></iframe>)
-}
+import Vid, { titleArry } from './Vid';
+//const cpyTitle = titleArry;
 
 const Videos  = () =>(
     <div>
         <h2>Videos tab</h2>
         <input type="text" className="input" placeholder="Search..." />
         <div> </div>
-        {videoArry}
+        {Vid}
     </div>
 )
 
